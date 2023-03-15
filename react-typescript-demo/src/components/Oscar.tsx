10e041f836de9224fb20448109c59cad143893b8
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-15 22:26:49 +0100

    Delete react-typescript-demo directory

--- react-typescript-demo/src/components/Oscar.tsx
+++
@@ -1,8 +0,0 @@
-type OscarProps = {
-    //This is how we defined children props 
-    children : React.ReactNode
-}
-
-export const Oscar = (props : OscarProps) => {
-    return <div>{props.children}</div>
-}
