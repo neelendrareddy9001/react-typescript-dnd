ac28fc0a66cebf5ee18b97fd15332dcc2f7b8120
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-19 22:06:00 +0100

    Update InputField.tsx

--- react-typescript-dnd/src/components/InputField.tsx
+++ react-typescript-dnd/src/components/InputField.tsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, {useRef} from 'react';
 import './styles.css';
 
 interface Props {
-    todo : string,
+    todo : string;
     setTodo : React.Dispatch<React.SetStateAction<string>>;
-}
-//const InputField:React.FC<Props> = ({todo, setTodo}) => {}
-const InputField = ({todo,setTodo} : Props ) => {
-  return (
-    <form className='input'>
-        <input 
-            type='input' 
-            placeholder='Enter a task' 
-            className='input__box'
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
-        />
-        <button type='submit' className='input_submit'>
-            Go
-        </button>
-    </form>
-  )
+    handleAdd : (e:React.FormEvent) => void;
 }
 
-export default InputField
+const InputField:React.FC<Props> = ({todo, setTodo,handleAdd}) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    return(
+        <form className='input' onSubmit={(e) => {
+            handleAdd(e)
+            inputRef.current?.blur();
+        }}>
+            <input 
+                type="text" 
+                placeholder='Enter a Task' 
+                className='input__box'
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+            />
+            <button type='submit' className='input_submit'>Go</button>
+        </form>
+    )
+}
+
+
+export default InputField;
