888aadc4bfc3a8cba5b9eac5479e4fff2c174505
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-19 23:12:37 +0100

    Update SingleTodo.tsx

--- react-typescript-dnd/src/components/SingleTodo.tsx
+++ react-typescript-dnd/src/components/SingleTodo.tsx
@@ -6,12 +6,13 @@ import './styles.css';
 
 
 type Props = {
+  index : number;
   todo : Todo;
   todos : Todo[];
   setTodos : React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const SingleTodo:React.FC<Props> = ({todo, todos, setTodos}) => {
+const SingleTodo:React.FC<Props> = ({index,todo, todos, setTodos}) => {
 
   const [edit, setEdit] = useState<boolean>(false);
   const [editTodo, setEditTodo] = useState<string>(todo.todo);
